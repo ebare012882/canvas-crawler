0dd05ad54a358c1936e7809d2aaa7bdb68d60107
Emily (Bare) Bernard <77028684+ebare012882@users.noreply.github.com>
2022-09-16 13:28:49 -0500

    added hero and ogre

--- js/main.js
+++ js/main.js
@@ -62,34 +62,59 @@ console.log('this is the game element', game)
 // notie this, the method uses function expression sybtax
 // using the keyword function, NOT an arrow function 
 // because one of the primary differences between a named function with the keyword and an arrow function is the context that 'this' refers to.
-const hero = {
-    x: 0,
-    y: 0,
-    color: 'hotpink',
-    width: 20,
-    height: 20, 
-    alive: true,
-    render: function() {
-        ctx.fillStyle = this.color
-        ctx.fillRect(this.x, this.y, this.width, this.height)
+// const hero = {
+//     x: 0,
+//     y: 0,
+//     color: 'hotpink',
+//     width: 20,
+//     height: 20, 
+//     alive: true,
+//     render: function() {
+//         ctx.fillStyle = this.color
+//         ctx.fillRect(this.x, this.y, this.width, this.height)
+//     }
+// }
+// // here's an example of how to make your hero show up on the canvas
+// hero.render()
+
+
+// // here's an example of an ogre object
+// const ogre = {
+//     x: 200,
+//     y: 100,
+//     color: "#bada55",
+//     width: 60,
+//     height: 120,
+//     alive: true,
+//     render: function () {
+//         ctx.fillStyle = this.color
+//         ctx.fillRect(this.x, this.y, this.width, this.height)
+//     }
+// }
+// // example of how to render the ogre
+// ogre.render()
+
+// because our two crawlers(hose are the guys that move around) are built of the same key value pairs, we can use a javascript class to build them. (JS class starts with a CAPITAL LETTER)
+class Crawler {
+    // we need a constructor function in order to make differences between instances of this class.
+    constructor(x, y, color, width, height) {
+        this.x= x,
+        this.y= y,
+        this.color= color,
+        this.width= width,
+        this.height= height,
+        // we can hard set values inside the constructor as well
+        this.alive = true,
+        // we can even set methods inside the constructor
+        this.render = function () {
+            ctx.fillStyle = this.color
+            ctx.fillRect(this.y, this.y, this.width, this.height)
+        }
     }
 }
-// here's an example of how to make your hero show up on the canvas
-hero.render()
 
+const player = new Crawler(10, 10, 'lightsteelblue', 16, 16)
+let ogre = new Crawler(200, 50, '#bada55', 32, 48)
 
-// here's an example of an ogre object
-const ogre = {
-    x: 200,
-    y: 100,
-    color: "#bada55",
-    width: 60,
-    height: 120,
-    alive: true,
-    render: function () {
-        ctx.fillStyle = this.color
-        ctx.fillRect(this.x, this.y, this.width, this.height)
-    }
-}
-
+player.render()
 ogre.render()
